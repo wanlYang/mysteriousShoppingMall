610b39778fd1775ac00819bcde37e31791204853
Yang <344295704@qq.com>
2020-04-17 14:49:23 +0800

    修改产品详情图片自适应

--- pages/product/detail.js
+++ pages/product/detail.js
@@ -25,7 +25,25 @@ Page({
     product:null,
     propertyValues:null,
     ceshiUrl:app.d.ceshiUrl,
-    reviews:null
+    reviews:null,
+    height:0
+  },
+  //设置轮播容器的高度
+  setContainerHeight: function (e) {
+    //图片的原始宽度
+    var imgWidth = e.detail.width;
+    //图片的原始高度
+    var imgHeight = e.detail.height;
+    //同步获取设备宽度
+    var sysInfo = wx.getSystemInfoSync();
+    //获取屏幕的宽度
+    var screenWidth = sysInfo.screenWidth;
+    //获取屏幕和原图的比例
+    var scale = screenWidth / imgWidth;
+    //设置容器的高度
+    this.setData({
+        height: imgHeight * scale
+    })
   },
   //页面加载完成函数
   onReady: function () {
